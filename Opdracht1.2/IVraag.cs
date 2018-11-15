aaeab58d3f366352e94213c79bffe2a4a7d5f914
alperenyavuz <kapikulefm@gmail.com>
2018-11-15 19:24:46 +0100

    Quiz class aangemaakt, Interface class IVraag aangemaakt

---
+++ Opdracht1.2/IVraag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht1._2
+{
+    interface IVraag
+    {
+        string Vraag { set; get; }
+        string Antwoord { set; get; }
+        int Graad { set; get; }
+        string Categorie { set; get; }
+    }
+}
